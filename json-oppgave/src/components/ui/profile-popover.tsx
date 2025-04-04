231f8dca44b302281b414f8d9a05b7e178030d06
jonous10 <jonous10@imkatta.no>
2025-04-04 12:54:15 +0200

    Add local storage of profile image

--- json-oppgave/src/components/ui/profile-popover.tsx
+++ json-oppgave/src/components/ui/profile-popover.tsx
@@ -2,27 +2,36 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
 } from "@/components/ui/popover"
 import { AvatarDemo } from "./profile"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export function ProfilePopover() {
-    const [imageSrc, setImageSrc] = useState<string>("")
-  return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <button><AvatarDemo src={imageSrc}/></button>
-      </PopoverTrigger>
-      <PopoverContent className="w-80">
-        <div className="grid gap-4">
-          <Input
-          onChange={(e) => setImageSrc(e.target.value)}
-          ></Input>
-        </div>
-      </PopoverContent>
-    </Popover>
-  )
+
+    const [imageSrc, setImageSrc] = useState<string>(
+        localStorage.getItem("localImage") || ""
+    )
+
+    useEffect(() => {
+        localStorage.setItem("localImage", imageSrc)
+    }, [imageSrc])
+
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <button><AvatarDemo src={imageSrc}/></button>
+            </PopoverTrigger>
+            <PopoverContent className="w-80">
+                <div className="grid gap-4">
+                    <Input
+                        value={imageSrc}
+                        onChange={(e) => setImageSrc(e.target.value)}
+                    />
+                </div>
+            </PopoverContent>
+        </Popover>
+    )
 }
