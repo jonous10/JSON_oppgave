86fd1749085b58f376cac85cb969e6ba49159a71
Schmockle <edward.svenkerud@gmail.com>
2025-04-04 09:44:25 +0200

    added new jookes

--- json-oppgave/src/app/edward/page.tsx
+++ json-oppgave/src/app/edward/page.tsx
@@ -3,26 +3,28 @@
 import { useEffect, useState } from "react";
 import Header from "@/components/ui/header";
 import list from "@/app/edward/list.json"; // Import JSON-filen med vitsene
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  const [jokes, setJokes] = useState<{}>([]);
+  const [jokes, setJokes] = useState<{}>([]); // State for å lagre vitsene
 
   useEffect(() => {
     // Sett vitsene fra JSON-filen i state
-    setJokes(list);
-  }, []);
+    setJokes(list); 
+  }, []); 
 
-  return (
-    <div className="">
+  return ( 
+    <div className=""> 
       <Header />
       <p>Mine beste jokes</p>
+      <Button>hawk</Button>
       <div>
-        {Object.entries(jokes).map(([key, value]: any) => (
-          <div key={key} className="flex flex-col items-center justify-center p-4">
-            <h2 className="text-2xl font-bold">{key}</h2>
-            <p className="text-lg">{value}</p>
-          </div>
-        ))}
+        {Object.entries(jokes).map(([key, value]: any) => (  
+          <div key={key} className="flex flex-col items-center justify-center p-4"> 
+            <h2 className="text-2xl font-bold">{key}</h2>  
+            <p className="text-lg">{value}</p>  
+          </div>  
+        ))}  
       </div>
     </div>
   );
