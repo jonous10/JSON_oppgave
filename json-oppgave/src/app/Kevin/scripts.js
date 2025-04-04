8400ac99209496df60696e76f990fd7e56196284
Metrizz1 <kevinlunde2004@gmail.com>
2025-04-04 08:36:26 +0200

    kommentarer i koden

--- json-oppgave/src/app/Kevin/scripts.js
+++ json-oppgave/src/app/Kevin/scripts.js
@@ -1,17 +1,17 @@
 let jokes = {};
-
+//her er det hvor scripten henter informasjonen/vitsene i fra JSON Filen 
 async function fetchJokes() {
    const response = await fetch('https://terjetheteacher.github.io/some-jokes/justJokes.json');
    jokes = await response.json();
    getRandomJoke();
 }
-
+//her er random funksjonen hvor denne henter ut helt tilfeldig informasjon/vist i fra JSON Filen
 function getRandomJoke(){
     const keys = Object.keys(jokes);
     const randomkey = keys[math.floor(math.random() * keys.length)];
     document.getElementById('joke').textContent = jokes[randomkey];
 }
-
+//dette er hvis alle funksjonen denne gjør sånn at den henter inn all informasjonen/vitsene i fra JSON Filen
 function showalljokes() {
     const alljokesDiv = document.getElementById('alljokes');
     alljokesDiv.innerHTML = Object.values(jokes).map(joke => `<p>- ${joke}</p>`).join('');
