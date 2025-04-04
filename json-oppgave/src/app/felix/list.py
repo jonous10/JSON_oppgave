f72d988396261c46561e1d27a8b2aefe62b2dc70
SharkFF2 <felfal14@imkatta.no>
2025-04-04 09:44:53 +0200

    add all jokes
    adds a function to print all the jokes

--- json-oppgave/src/app/felix/list.py
+++ json-oppgave/src/app/felix/list.py
@@ -2,14 +2,24 @@ import json
 import requests
 import random
 
+# URL to the JSON file containing jokes
 URL = "https://terjetheteacher.github.io/some-jokes/justJokes.json"
+
 
 def get_joke_1():
     respons = requests.get(URL)
     return respons.json()
 
+
+
+
+def show_all_jokes(jokes):
+    print("\nAll jokes:")
+    for i, joke in enumerate(jokes, start=1):
+        print(f"Joke {i}: {joke}")
+  
+
 vitser = get_joke_1()
 jokes = list(vitser.values())
-first_joke = jokes[0]
-print(first_joke)
 
+show_all_jokes(jokes)
