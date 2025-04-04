df73c5b2d6208333d538d9679415abb7ec79876f
SharkFF2 <felfal14@imkatta.no>
2025-04-04 08:50:24 +0200

    Add a working jokes program

--- json-oppgave/src/app/felix/list.py
+++ json-oppgave/src/app/felix/list.py
@@ -1,64 +1,67 @@
-{
-    "metadata": "A collection of jokes to be used freely as a practice for students learning about JavaScript Object notation (JSON).",
-    "jokes":[       {
-            "id" : "1",
-            "joke" : "where do generals keep their armies? In their sleevies!"
-        },
-        {
-            "id" : "2",
-            "joke" : "i'm thinking of reasons to go to switzerland. The flag is a big plus."
-        },
-        {
-            "id" : "3",
-            "joke" : "i didn't like my beard at first. Then it grew on me."
-        },
-        {
-            "id" : "4",
-            "joke" : "why don't you starv to death in the desert? Because of all the sand which is there."
-        },
-        {
-                "id" : "5",
-                "joke" : "why can't a bicycle stand up by itself? Because its two tired."
-        },
-        {
-                "id" : "6",
-                "joke" : "found out i was color blind the other day. That one came out of the orange."
-        },
-        {
-            "id" : "7",
-            "joke" : "A burger walks into a bar. The bartender says, 'Sorry, we don't server food here.'"
-        },
-        {
-            "id" : "8",
-            "joke" : "What's more amazing than a talking dog? A spelling bee."
-        },
-        {
-            "id" : "9",
-            "joke" : "Orion's Betl was a huge waist of space."
-        },
-        {
-            "id" : "10",
-            "joke" : "Why was 6 afraid of 7. Because 7, 8, 9!"
-        },
-        {
-            "id" : "11",
-            "joke" : "There are only 10 types of people in this world: Those who understand the binary, and those who don't."
-        },
-        {
-            "id" : "12",
-            "joke" : "Why did the scarecrow win an award? Because he was outstanding in his field."
-        },
-        {
-            "id" : "13",
-            "joke" : "I used to play piano by ear, but now I use my hands."
-        },
-        {
-            "id" : "14",
-            "joke" : "Why don't skeletons fight each other? They don't have the guts."
-        },
-        {
-            "id" : "15",
-            "joke" : "I would tell you a joke about an elevator, but it's an uplifting experience."
-        }
+import requests
+import json
+import random
 
-}
+# URL til JSON-filen
+URL = "https://terjetheteacher.github.io/some-jokes/justJokes.json"
+
+# Hent JSON-data fra URL
+def hent_json(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print("Feil ved henting av data:", e)
+        return None
+
+# Vis første vits
+def vis_vits_1(vitser):
+    jokes = list(vitser.values())
+    if jokes:
+        print("\nVits nr. 1:")
+        print(jokes[0])
+    else:
+        print("Fant ingen vitser.")
+
+# Vis alle vitser
+def vis_alle_vitser(vitser):
+    print("\nAlle vitser:")
+    for key, vits in vitser.items():
+        print("-" * 40)
+        print(f"vits{key}: {vits}")
+
+# Vis en tilfeldig vits
+def vis_tilfeldig_vits(vitser):
+    if vitser:
+        print("\nTilfeldig vits:")
+        print(random.choice(list(vitser.values())))
+    else:
+        print("Ingen vitser tilgjengelig.")
+
+# Hovedmeny
+def meny(vitser):
+    while True:
+        print("\nMeny:")
+        print("1: Vis første vits")
+        print("2: Vis alle vitser")
+        print("3: Vis en tilfeldig vits")
+        print("4: Avslutt")
+        valg = input("Velg et alternativ: ")
+
+        if valg == "1":
+            vis_vits_1(vitser)
+        elif valg == "2":
+            vis_alle_vitser(vitser)
+        elif valg == "3":
+            vis_tilfeldig_vits(vitser)
+        elif valg == "4":
+            print("Avslutter...")
+            break
+        else:
+            print("Ugyldig valg, prøv igjen.")
+
+# Kjør programmet
+vitser = hent_json(URL)
+if vitser:
+    meny(vitser)
